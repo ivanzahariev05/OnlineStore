518966a04de06cda8402665eadc940bafd2213f2
ivanzahariev05 <ivanzaxariev2005@abv.bg>
2025-03-07 09:13:49 +0200

    New fields added

--- src/main/java/softuni/bg/supplementsonlinestore/transaction/model/Transaction.java
+++ src/main/java/softuni/bg/supplementsonlinestore/transaction/model/Transaction.java
@@ -2,15 +2,19 @@ package softuni.bg.supplementsonlinestore.transaction.model;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import softuni.bg.supplementsonlinestore.user.model.User;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.Currency;
 import java.util.UUID;
 
 @Entity
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "transactions")
@@ -24,10 +28,23 @@ public class Transaction {
     private BigDecimal amount;
 
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private TransactionType type;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TransactionStatus status;
 
     @Column(nullable = false)
     private LocalDateTime transactionDate;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User sender;
+
+    private Currency currency;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User receiver;
 
 }
+

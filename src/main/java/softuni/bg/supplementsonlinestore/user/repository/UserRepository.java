08ff643ad904a567df8d8978e27d5e007a67e968
ivanzahariev05 <ivanzaxariev2005@abv.bg>
2025-03-07 09:07:30 +0200

    Repositories modified

--- src/main/java/softuni/bg/supplementsonlinestore/user/repository/UserRepository.java
+++ src/main/java/softuni/bg/supplementsonlinestore/user/repository/UserRepository.java
@@ -4,13 +4,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import softuni.bg.supplementsonlinestore.user.model.User;
 
+
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
-public interface UserRepository  extends JpaRepository<User, Long> {
+public interface UserRepository  extends JpaRepository<User, UUID> {
 
-    Optional<User> findByUsername(String username);
+    Optional<User> findByUsername(java.lang.String username);
 
-    Optional<User> findByEmail(String email);
+    Optional<User> findByEmail(java.lang.String email);
+
+    Optional<User> findUserById(UUID id);
 
 }
--- src/main/java/softuni/bg/supplementsonlinestore/wallet/repository/WalletRepository.java
+++ src/main/java/softuni/bg/supplementsonlinestore/wallet/repository/WalletRepository.java
@@ -4,8 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import softuni.bg.supplementsonlinestore.wallet.model.Wallet;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface WalletRepository extends JpaRepository<Wallet, UUID> {
+
+    Optional<Wallet> findWalletById(UUID id);
 }

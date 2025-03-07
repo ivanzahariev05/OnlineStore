d49c787cfe3fb4a2ad8492a9bd089659abd7a3a2
ivanzahariev05 <ivanzaxariev2005@abv.bg>
2025-03-07 09:11:47 +0200

    2 dtos added

---
+++ src/main/java/softuni/bg/supplementsonlinestore/web/dto/SendToAFriendRequest.java
@@ -0,0 +1,22 @@
+package softuni.bg.supplementsonlinestore.web.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class SendToAFriendRequest {
+
+    @NotNull
+    private String username;
+
+    @NotNull
+    private BigDecimal amount;
+}

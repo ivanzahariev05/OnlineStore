29a52c42483d67957f7f80f7a66ff587f24802c2
ivanzahariev05 <ivanzaxariev2005@abv.bg>
2025-03-07 09:12:55 +0200

    Configuration modified

--- src/main/java/softuni/bg/supplementsonlinestore/config/Config.java
+++ src/main/java/softuni/bg/supplementsonlinestore/config/Config.java
@@ -3,7 +3,10 @@ package softuni.bg.supplementsonlinestore.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.time.format.DateTimeFormatter;
 
 @Configuration
 public class Config {
@@ -12,7 +15,6 @@ public class Config {
     public PasswordEncoder passEncoder() {
         return new BCryptPasswordEncoder();
     }
-
 
 
 }

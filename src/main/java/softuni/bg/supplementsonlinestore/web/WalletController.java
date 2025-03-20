c8fa5c964b79301752d6ef96dd49e12fc97d28c5
ivanzahariev05 <ivanzaxariev2005@abv.bg>
2025-03-21 00:18:06 +0200

    GetWallet fix because of transactions

--- src/main/java/softuni/bg/supplementsonlinestore/web/WalletController.java
+++ src/main/java/softuni/bg/supplementsonlinestore/web/WalletController.java
@@ -1,20 +1,21 @@
 package softuni.bg.supplementsonlinestore.web;
 
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import softuni.bg.supplementsonlinestore.security.MetaDataAuthentication;
 import softuni.bg.supplementsonlinestore.transaction.model.Transaction;
+import softuni.bg.supplementsonlinestore.transaction.model.TransactionType;
 import softuni.bg.supplementsonlinestore.transaction.service.TransactionService;
 import softuni.bg.supplementsonlinestore.user.model.User;
 import softuni.bg.supplementsonlinestore.user.service.UserService;
-import softuni.bg.supplementsonlinestore.wallet.model.Wallet;
 import softuni.bg.supplementsonlinestore.wallet.service.WalletService;
 import softuni.bg.supplementsonlinestore.web.dto.AddFundsRequest;
 import softuni.bg.supplementsonlinestore.web.dto.SendToAFriendRequest;
@@ -24,6 +25,7 @@ import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Controller
 @RequestMapping("/wallet")
 public class WalletController {
@@ -43,20 +45,21 @@ public class WalletController {
         ModelAndView modelAndView = new ModelAndView("wallet");
         User user = userService.findById(metaDataAuthentication.getId());
 
-        List<Transaction> limitedTransactions = user.getTransactions()
+        List<Transaction> transactions = transactionService.findByUser(user.getUsername())
                 .stream()
+                .filter(t -> (t.getType() == TransactionType.DEPOSIT && t.getOwner().equals(user.getUsername())) ||
+                        (t.getType() != TransactionType.DEPOSIT && t.getSender().equals(user.getUsername())))
                 .sorted(Comparator.comparing(Transaction::getTransactionDate).reversed())
                 .limit(8)
                 .collect(Collectors.toList());
 
-        user.setTransactions(limitedTransactions);
-
+        modelAndView.addObject("transactions", transactions);
         modelAndView.addObject("user", user);
         return modelAndView;
     }
 
 
-    @PutMapping("/{id}/add-funds")
+    @PostMapping("/{id}/add-funds")
     public String addFunds(@PathVariable UUID id, @Valid AddFundsRequest addFundsRequest, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             return "wallet";
@@ -66,14 +69,17 @@ public class WalletController {
         return "redirect:/wallet";
     }
 
-    @PutMapping("/send")
+    @PostMapping("/send")
     public String sendToAFriend(@Valid SendToAFriendRequest sendToAFriendRequest, BindingResult bindingResult) {
+        log.info("Send request received: {}", sendToAFriendRequest);
+
         if (bindingResult.hasErrors()) {
+            log.error("Validation errors: {}", bindingResult.getAllErrors());
             return "wallet";
         }
+
         walletService.sendToAFriend(sendToAFriendRequest);
         return "redirect:/wallet";
-
     }
 
 }
